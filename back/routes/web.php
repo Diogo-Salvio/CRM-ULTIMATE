3f10b67d9d09d718b0ae88971ff2287d76321581
Diogo Salvio <diogosalvio787@gmail.com>
2025-11-10 20:24:01 -0300

    feat: instalaçao do back end laravel

---
+++ back/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Este arquivo está vazio pois esta aplicação é apenas uma API backend.
+| Todas as rotas devem ser registradas em routes/api.php
+|
+*/
